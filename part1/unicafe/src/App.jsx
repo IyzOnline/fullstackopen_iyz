0f540731424be63a223078e511d13cd3f276ef35
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-09-13 05:33:23 +0800

    feat: Create Button component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
 
+const Button = ({ onClick, text }) => {
+  <button onClick={onClick}>
+    {text}
+  </button>
+}
+
 const App = () => {
-  // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   return (
     <div>
-      code here
+      
     </div>
   )
 }
