62e498f891fab66dae66ce789ce36de1b191777f
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-09-13 06:14:25 +0800

    feat: Display total, average, and positive values

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -9,12 +9,16 @@ const Button = ({ onClick, text }) => {
 }
 
 const Statistics = ({ values }) => {
+  const total = values.reduce((acc, cur) => acc + cur, 0)
   return (
     <div>
       <h1>Statistics</h1>
       <p>good {values[0]}</p>
       <p>neutral {values[1]}</p>
       <p>bad {values[2]}</p>
+      <p>all {total}</p>
+      <p>average {total / values.length}</p>
+      <p>positive {values[0] / (total ? total : 1)} %</p>
     </div>
   )
 }
