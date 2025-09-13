ce4b03822984d7401f78154cb582eebc54f85d2e
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-09-13 23:15:52 +0800

    feat: Return table rows in Stat...Line component instead

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,7 +8,14 @@ const Button = ({ onClick, text }) => {
   )
 }
 
-const StatisticLine = ({ text, value }) => <p>{text} {value}</p>
+const StatisticLine = ({ text, value }) => {
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
 
 const Statistics = ({ values }) => {
   const total = values.reduce((acc, cur) => acc + cur, 0)
