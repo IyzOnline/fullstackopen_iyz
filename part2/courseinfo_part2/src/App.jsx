25e868904a7a48146b8f3930ce462a8fcc131de1
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-09-17 14:17:54 +0800

    feat: Import Course component into App

--- part2/courseinfo_part2/src/App.jsx
+++ part2/courseinfo_part2/src/App.jsx
@@ -1,16 +1,4 @@
-const Course = ({ course }) => {
-  return (
-    <div>
-      <h2>{course.name}</h2>
-      {course.parts.map((part) => (
-        <p key={part.id}>
-          {part.name} {part.exercises}
-        </p>
-      ))}
-      <p><b>total of {course.parts.reduce((acc, cur) => acc + cur.exercises, 0)} exercises</b></p>
-    </div>
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
