5e35016098d2c5fb13b6260775a3127104fcb292
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-09-17 14:11:38 +0800

    feat: Display each course from courses array in App

--- part2/courseinfo_part2/src/App.jsx
+++ part2/courseinfo_part2/src/App.jsx
@@ -1,6 +1,6 @@
 const Course = ({ course }) => {
   return (
-    <div key={course.id}>
+    <div>
       <h1>{course.name}</h1>
       {course.parts.map((part) => (
         <p key={part.id}>
@@ -13,29 +13,56 @@ const Course = ({ course }) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      }
-    ]
-  }
+  const courses = [
+    {
+      name: 'Half Stack application development',
+      id: 1,
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    }, 
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  return <Course course={course} />
+  return (
+    <div>
+      {courses.map(courseInstance => <Course key={courseInstance.id} course={courseInstance}/>)}
+    </div>
+  )
 }
 
 export default App
