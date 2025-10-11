bf6a16b9cf9362f85b1239ac32213085a6a7b276
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-10-11 11:23:25 +0800

    fix: A single exact match should be displayed if there are multiple matches based on substring

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -17,6 +17,21 @@ function App() {
     }
   }, [])
 
+  const filterResults = (inputFilter) => {
+    let filterResult = countryNames.filter(country => country.name.toLowerCase().includes(inputFilter))
+    if (filterResult.length > 10) {
+      setCountryResults("Too many matches, specify another filter")
+    } 
+    else if (filterResult.length > 1) {
+      const singleResultFilter = filterResult.filter(country => country.name.toLowerCase() === inputFilter)
+      if (singleResultFilter.length === 1) filterResult = singleResultFilter
+      setCountryResults(filterResult)
+    }
+    else if (filterResult.length === 1) {
+      setCountryResults([filterResult[0]])
+    }
+  }
+
   const handleChange = (event) => {
     setInputCountry(event.target.value)
     const lowerCaseTrimmedFilter = event.target.value.toLowerCase().trim()
@@ -25,16 +40,7 @@ function App() {
       return
     }
 
-    const filterResult = countryNames.filter(country => country.name.toLowerCase().includes(lowerCaseTrimmedFilter))
-    if (filterResult.length > 10) {
-      setCountryResults("Too many matches, specify another filter")
-    } 
-    else if (filterResult.length > 1) {
-      setCountryResults(filterResult)
-    }
-    else if (filterResult.length === 1) {
-      setCountryResults([filterResult[0]])
-    }
+    filterResults(lowerCaseTrimmedFilter)
   }
   
   if (countryNames) {
