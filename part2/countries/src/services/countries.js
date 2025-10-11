cd39ddc8b6e6d27e2032437727ff79098a16d0fd
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-10-11 10:26:49 +0800

    feat: Get data from API and return name and key for each country

--- part2/countries/src/services/countries.js
+++ part2/countries/src/services/countries.js
@@ -2,7 +2,16 @@ import axios from 'axios'
 const baseURL = "https://studies.cs.helsinki.fi/restcountries/api/all"
 
 const getAllCountryNames = () => {
-    return axios.get(baseURL).then(response => response.data).then(data => data.map(country => country.name.common))
+    return axios.
+    get(baseURL)
+    .then(response => response.data)
+    .then(data => data.map(country => {
+        return { 
+            name: country.name.common, 
+            key: country.ccn3
+        }
+    }
+    ))
 }
 
 export default { getAllCountryNames }
