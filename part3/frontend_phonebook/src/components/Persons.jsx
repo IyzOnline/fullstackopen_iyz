009a59ea5e2d99f42e9f5a329b37287627d3c324
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-10-31 01:36:37 +0800

    feat: Add notif message updating when deleting person data

--- part3/frontend_phonebook/src/components/Persons.jsx
+++ part3/frontend_phonebook/src/components/Persons.jsx
@@ -1,10 +1,24 @@
-const Persons = ({ deletePerson, persons, setPersons, filterName }) => {
+const Persons = ({ setMessage, deletePerson, persons, setPersons, filterName }) => {
     const deleteHandler = person => {
         if (window.confirm(`Do you wish to delete "${person.name}" from your contacts?`)) {
             deletePerson(person.id)
                 .then(() => {
                     const updatedPersons = persons.filter(currentPerson => currentPerson.id !== person.id)
                     setPersons(updatedPersons)
+                    setMessage({
+                        message: `Removed ${person.name}`,
+                        messageType: "success"
+                    })
+                })
+                .catch(error => {
+                    console.log(error)
+                    setMessage({
+                        message: error.response.data.error,
+                        messageType: "error"
+                    })
+                    setInterval(() => {
+                        setMessage(null)
+                    }, 5000)
                 })
         }
     }
