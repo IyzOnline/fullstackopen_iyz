d0c9ae37345063f5c18dcf198c3ae40be03631b4
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-10-31 01:40:04 +0800

    fix: Return error message when status code is 500 if it exists

--- part3/backend_phonebook/index.js
+++ part3/backend_phonebook/index.js
@@ -6,30 +6,6 @@ const person = require('./models/person')
 
 const app = express()
 
-let persons = 
-[
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 app.use(express.static('dist'))
 app.use(express.json())
 
@@ -104,7 +80,6 @@ app.post('/api/persons', (request, response, next) => {
 
   if (!newPersonDetails.name || !newPersonDetails.number) {
     const error = new Error('Person name or number is missing');
-    error.status = 400;
     error.name = 'ValidationError';
 
     return next(error)
@@ -166,17 +141,15 @@ app.put('/api/persons/:id', (request, response, next) => {
 })
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+  console.error(error)
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: 'malformed id '})
-  } 
-  
-  if (error.name === "ValidationError") {
-    return response.status(error.status).json({ error: error.message })
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message })
   }
 
-  return response.status(500).json({ error: 'something went wrong' })
+  return response.status(500).json({ error: error.message || "something went wrong"})
 }
 
 app.use(errorHandler)
