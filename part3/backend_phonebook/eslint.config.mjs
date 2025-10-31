d3281ff8b6d6a8dfcab31c6dbdf8ca220806ea4e
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-10-31 17:28:19 +0800

    fix: Edit eslint config file to match lint rules

--- part3/backend_phonebook/eslint.config.mjs
+++ part3/backend_phonebook/eslint.config.mjs
@@ -1,21 +1,21 @@
-import globals from "globals";
+import globals from 'globals'
 import js from '@eslint/js'
-import { defineConfig } from "eslint/config";
+import { defineConfig } from 'eslint/config'
 import stylistic from '@stylistic/eslint-plugin'
-import stylisticJs from '@stylistic/eslint-plugin-js';
+import stylisticJs from '@stylistic/eslint-plugin-js'
 
 export default defineConfig([
   js.configs.recommended,
   stylistic.configs.customize({
     indent: 2,
-    quotes: single,
+    quotes: 'single',
     semi: false,
   }),
-  { 
-    files: ["**/*.js"], 
-    languageOptions: { 
-      sourceType: "commonjs",
-      globals: { ...globals.node }, 
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+      globals: { ...globals.node },
       ecmaVersion: 'latest',
     },
     plugins: {
@@ -23,11 +23,11 @@ export default defineConfig([
     },
     rules: {
       '@stylistic/js/linebreak-style': ['error', 'unix'],
-      eqeqeq: 'error',
+      'eqeqeq': 'error',
       'no-console': 'off',
-    }
+    },
   },
   {
     ignores: ['dist/**'],
-  }
-]);
+  },
+])
