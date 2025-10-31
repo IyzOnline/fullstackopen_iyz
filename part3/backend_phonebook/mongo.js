2a19f4987721407fb6c812a7815f77df8ca26801
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-10-31 17:59:39 +0800

    docu: Answer to 3.22 - Finally, edit mongo.js to match lint rules

--- part3/backend_phonebook/mongo.js
+++ part3/backend_phonebook/mongo.js
@@ -1,44 +1,43 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
 if (process.argv.length < 5 && process.argv.length !== 3) {
-    console.log("Need more arguments");
-    process.exit(1);
+  console.log('Need more arguments')
+  process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
-const url = `mongodb+srv://johnisaacbarbosanaga_db_user:${password}@cluster0.0koozst.mongodb.net/?appName=Cluster0`;
+const url = `mongodb+srv://johnisaacbarbosanaga_db_user:${password}@cluster0.0koozst.mongodb.net/?appName=Cluster0`
 
-mongoose.set('strictQuery', false);
-mongoose.connect(url);
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
-});
+  name: String,
+  number: String,
+})
 
-const Person = new mongoose.model('Person', personSchema);
+const Person = new mongoose.model('Person', personSchema)
 
 if (process.argv.length === 3) {
-    Person
-        .find({})
-        .then(response => {
-            console.log("phonebook:");
-            response.map(person => console.log(`${person.name} ${person.number}`))
-            mongoose.connection.close();
-        });
-    
+  Person
+    .find({})
+    .then((response) => {
+      console.log('phonebook:')
+      response.map(person => console.log(`${person.name} ${person.number}`))
+      mongoose.connection.close()
+    })
 } else if (process.argv.length === 5) {
-    const name = process.argv[3];
-    const number = process.argv[4];
+  const name = process.argv[3]
+  const number = process.argv[4]
 
-    const person = new Person({
-        name: name,
-        number: number,
-    });
+  const person = new Person({
+    name: name,
+    number: number,
+  })
 
-    person.save().then(result => {
-        console.log(`${person.name} ${person.number}`)
-        mongoose.connection.close();
-    });
+  person.save().then(() => {
+    console.log(`${person.name} ${person.number}`)
+    mongoose.connection.close()
+  })
 }
