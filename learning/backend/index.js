1a39bea383fc3790ac401a1ff4fa6a145d5ca534
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-10-31 18:00:00 +0800

    Part 3 Done!

--- learning/backend/index.js
+++ learning/backend/index.js
@@ -109,5 +109,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`) 
+  console.log(`Server running on port ${PORT}`)
 })
