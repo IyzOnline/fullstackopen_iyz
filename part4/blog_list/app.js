bbdb697541b7ee97cfdb30a37263aa9929c9d12a
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-11-01 22:52:11 +0800

    docu: Answer to 4.1 - 4.2

---
+++ part4/blog_list/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const config = require('./utils/config.js')
+const logger = require('./utils/logger.js')
+const blogsRouter = require('./controllers/blogs.js')
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('Successfully connected to database.')
+  })
+  .catch(error => {
+    logger.error('error connecting to MongoDB: ', error.message)
+  })
+
+const app = express()
+
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
