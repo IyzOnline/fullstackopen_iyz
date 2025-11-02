de6a253120f915ac527f32494005171eb1b53549
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-11-02 17:04:30 +0800

    docu: Answer to 4.4
    Create tests within the describe block aimed at obtaining the sum of
    the likes of all blogs within a given list.

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -2,6 +2,16 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reduce = (sum, item) => {
+    console.log('item likes:', item.likes)
+    return sum + item.likes
+  }
+
+  return blogs.reduce(reduce, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
