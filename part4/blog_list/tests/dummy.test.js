433540c0b8e3fef11baa253c88d177c89f9f91cb
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-11-05 07:58:02 +0800

    docu: Fix spacings

--- part4/blog_list/tests/dummy.test.js
+++ part4/blog_list/tests/dummy.test.js
@@ -85,18 +85,18 @@ const listWithTenBlogs = [
     }
   ]
 
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
-      likes: 5,
-      __v: 0
-    }
-  ]
+const listWithOneBlog = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+    likes: 5,
+    __v: 0
+  }
+]
 
-  const listWithNoBlogs = []
+const listWithNoBlogs = []
 
 test('dummy returns one', () => {
   const result = listHelper.dummy(listWithNoBlogs)
