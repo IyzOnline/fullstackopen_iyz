499ce8c3a6709a9927030bba5d7e758b506b8bc3
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-11-06 05:51:50 +0800

    feat: Use error handler middleware in the express app

--- part4/blog_list/app.js
+++ part4/blog_list/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const config = require('./utils/config.js')
 const logger = require('./utils/logger.js')
 const blogsRouter = require('./controllers/blogs.js')
+const middleware = require('./utils/middleware.js')
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
@@ -17,5 +18,7 @@ const app = express()
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+
+app.use(middleware)
 
 module.exports = app
