ca42247a40db59518d1a0ae39d399d5cf1ead333
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-11-06 10:05:43 +0800

    feat: Add route for updating blog info

--- part4/blog_list/controllers/blogs.js
+++ part4/blog_list/controllers/blogs.js
@@ -1,4 +1,5 @@
 const blogsRouter = require('express').Router()
+const note = require('../../../learning/backend/models/note.js')
 const Blog = require('../models/blog.js')
 
 blogsRouter.get('/', async (request, response) => {
@@ -17,6 +18,20 @@ blogsRouter.post('/', async (request, response) => {
 blogsRouter.delete('/:id', async (request, response) => {
   await Blog.findByIdAndDelete(request.params.id)
   response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  console.log("This is the req body: ", request.body)
+  const noteCheck = await Blog.findById(request.params.id)
+
+  for (key of Object.keys(noteCheck)) {
+    if (request.body[key]) {
+      noteCheck[key] = request.body[key]
+    }
+  }
+
+  await noteCheck.save()
+  response.status(200).json(noteCheck)
 })
 
 module.exports = blogsRouter
