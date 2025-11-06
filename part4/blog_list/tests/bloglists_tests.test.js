1c3d9496b26d7e297801ded91b315e082a4f03bf
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-11-06 10:05:14 +0800

    feat: Test updating blog information

--- part4/blog_list/tests/bloglists_tests.test.js
+++ part4/blog_list/tests/bloglists_tests.test.js
@@ -81,6 +81,62 @@ test('blog is deleted with status code 204', async () => {
     assert.strictEqual(resultingBlogs.length, elpers.blogsForTesting.length - 1)
 })
 
+describe('updating a blog given author name', () => {
+  test('update likes only, but given author and likes', async () => {
+    const updateDetails = {
+      author: "Ohnepixel",
+      likes: 100
+    }
+
+    const blogToUpdate = await Blog.findOne({ author: updateDetails.author})
+
+    const updatedBlog = await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(updateDetails)
+      .expect(200)
+
+    assert.strictEqual(updatedBlog.likes, updateDetails.likes)
+  })
+
+  test('update likes and url, but given author, url, and likes', async () => {
+    const updateDetails = {
+      author: "Ohnepixel",
+      url: "NEINNEINNEIN",
+      likes: 9
+    }
+
+    const blogToUpdate = await Blog.findOne({ author: updateDetails.author })
+
+    const updatedBlog = await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(updateDetails)
+      .expect(200)
+
+    assert.strictEqual(updatedBlog.likes, updateDetails.likes)
+    assert.strictEqual(updatedBlog.url, updateDetails.url)
+  })
+
+  test('update likes, url, and title, but given author, url, likes, and title', async () => {
+    const updateDetails = {
+      title: "RED CRIMSON KIMONOS?!?!?",
+      author: "Ohnepixel",
+      url: "NEINNEINNEIN",
+      likes: 9
+    }
+
+    const blogToUpdate = await Blog.findOne({ author: updateDetails.author })
+
+    const updatedBlog = await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(updateDetails)
+      .expect(200)
+
+    assert.strictEqual(updatedBlog.likes, updateDetails.likes)
+    assert.strictEqual(updatedBlog.url, updateDetails.url)
+    assert.strictEqual(updatedBlog.title, updateDetails.title)
+  })
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
